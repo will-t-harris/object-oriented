3fe0f9a2c3462c1be248fa4eeb365b8234fa31b5
Will Harris <will.t.harris@protonmail.com>
2019-07-15 15:47:06 -0600

    finish comments for state variables on Author class

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -10,10 +10,34 @@ require_once("");
  * @package Wharris21\ObjectOriented
  */
 class Author {
+	/**
+	 * id for this Author; this is the primary key
+	 * @var Uuid $authorId
+	 */
 	private $authorId;
+	/**
+	 * url for the author's avatar image
+	 * @var string $authorAvatarUrl;
+	 */
 	private $authorAvatarUrl;
+	/**
+	 * string value for author's account activation/verification
+	 * @var string $authorActivationToken;
+	 */
 	private $authorActivationToken;
+	/**
+	 * string value for author's email address
+	 * @var string $authorEmail;
+	 */
 	private $authorEmail;
+	/**
+	 * string value to verify author's password
+	 * @var string $authorHash;
+	 */
 	private $authorHash;
+	/**
+	 * string value for author's username
+	 * @var string $authorUsername;
+	 */
 	private $authorUsername;
 }
