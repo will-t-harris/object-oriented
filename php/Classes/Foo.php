f3cf160b60ad4d953616f32ba2c1bd081a9b5e05
Will Harris <will.t.harris@protonmail.com>
2019-07-15 15:34:04 -0600

    comment author class

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -3,6 +3,12 @@ namespace Wharris21\ObjectOriented;
 
 require_once("autoload.php");
 require_once("");
+
+/**
+ * Class Author
+ * This class will contain all of the state variables and methods for any instance of Author
+ * @package Wharris21\ObjectOriented
+ */
 class Author {
 	private $authorId;
 	private $authorAvatarUrl;
