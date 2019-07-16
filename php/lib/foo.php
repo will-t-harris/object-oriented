a721bf1ecf25b4593861407d76d2adc2f18971df
Will Harris <will.t.harris@protonmail.com>
2019-07-16 16:24:50 -0600

    rename Foo.php to Author.php, remove use statment in foo.php

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -1,6 +1,6 @@
 <?php
 namespace Wharris21\ObjectOriented;
 
-use Wharris21\ObjectOriented\Author;
-
 $will = new Author("d749f0ca428141b092a6437e75ec5b03", "http://blog.eu.playstation.com/files/avatars/avatar_166315.jpg", "abc123xyz789", "wharris21@cnm.edu", "ee22ce27d86b80a75fcc7430284b7764", "wharris21");
+
+print_r($will);
