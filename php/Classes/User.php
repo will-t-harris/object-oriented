b59a7de427ce9cea1b1a3535d87d4c558931c921
Will Harris <will.t.harris@protonmail.com>
2019-07-21 11:09:25 -0600

    add requires and begin defining User class

--- php/Classes/User.php
+++ php/Classes/User.php
@@ -1,2 +1,31 @@
 <?php
 namespace Wharris21\ObjectOriented;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
+/**
+ * User class
+ * this class will contain all of the state variables and methods for any instance of the User class
+ *
+ *
+ **/
+class User implements \JsonSerializable {
+	use ValidateUuid;
+
+	/**
+	 * id for this User; this is the primary key
+	 * @var Uuid $userId
+	 */
+	private $userId;
+	/**
+	 * hash for this User; argon2i string
+	 * @var string $userHash
+	 */
+	private $userHash;
+	private $userLocation;
+	private $userEmail;
+	private $userPhoneNumber;
+}
