9562095871396b5727df2fedf2696ba1357d1259
Will Harris <will.t.harris@protonmail.com>
2019-07-21 10:58:35 -0600

    change authorHash to correct argon2i format

--- php/lib/author.php
+++ php/lib/author.php
@@ -3,6 +3,6 @@ namespace Wharris21\ObjectOriented;
 
 require_once("../Classes/Author.php");
 
-$will = new Author("379dae82-5a2b-4c4b-8193-b8e7749a3495", "http://blog.eu.playstation.com/files/avatars/avatar_166315.jpg", "abc123xyz789", "wharris21@cnm.edu", "8pasmvh7Ebw2XT2KGd8xy6hYEW1jzzHGTzl8KUu5azxuOjWcpOq0Ds9ksg58n481LWNlKvBM4MLWFfq1e9eBh3jQXf6lT3GeS", "wharris21");
+$will = new Author("379dae82-5a2b-4c4b-8193-b8e7749a3495", "http://blog.eu.playstation.com/files/avatars/avatar_166315.jpg", "abc123xyz789", "wharris21@cnm.edu", '$argon2i$v=19$m=1024,t=384,p=2$dE55dm5kRm9DTEYxNFlFUA$nNEMItrDUtwnDhZ41nwVm7ncBLrJzjh5mGIjj8RlzVA', "wharris21");
 
 var_dump($will);
