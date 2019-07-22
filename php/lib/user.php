6c4e289f5e462b01a6d3fb5ca6d984fba22adb3e
Will Harris <will.t.harris@protonmail.com>
2019-07-22 16:23:11 -0600

    update user instantiation file

--- php/lib/user.php
+++ php/lib/user.php
@@ -3,6 +3,6 @@ namespace Wharris21\ObjectOriented;
 
 require_once("../Classes/User.php");
 
-$user = new User();
+$user = new User("379dae82-5a2b-4c4b-8193-b8e7749a3495", '$argon2i$v=19$m=1024,t=384,p=2$dE55dm5kRm9DTEYxNFlFUA$nNEMItrDUtwnDhZ41nwVm7ncBLrJzjh5mGIjj8RlzVA', "abc123xyz789", "wharris21@cnm.edu", "505-555-5555");
 
 var_dump($user);
